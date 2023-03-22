4ab28c28f4b5725abdb660ab05355a7e45a6ea28
michael young <neuromyoung@gmail.com>
2023-03-21 20:09:27 -0400

    add interactivity

--- dashboard.py
+++ dashboard.py
@@ -7,97 +7,141 @@ st.set_page_config(layout="wide")
 
 st.markdown("# Sales Dashboard")
 
-sheet = st.file_uploader("Upload Excel Spreadsheet")
+sheet = st.file_uploader("Upload Excel Spreadsheet", accept_multiple_files = True)
 st.write("#")
-st.markdown("## Filter Data")
-ca, cb, cc, cd, ce, cf = st.columns((1, 1, 1, 1, 1, 1))
-st.write("#")
-c1, c2, c3, c4, c5 = st.columns((5, 1,5, 1,5))
+
+tab1, tab2 = st.tabs(["Revenue", "Expenses"])
 
 
-if sheet is not None:
-    df = pd.read_excel(sheet, sheet_name="Sales", header = None)  
-    header_idx = df[df.eq("Item").any(1)].index.values[0]
-    footer_idx = df[df.eq("Total").any(1)].index.values[0]-1
+if len(sheet) > 0:
+    with tab1:
+        st.markdown("## Select Date Range")
+        ca, cb, cc, cd, ce, cf = st.columns((1, 1, 1, 1, 1, 1))
+        st.write("#")
+        c1, c2, c3, c4, c5 = st.columns((5, 1,5, 1,5))
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-    ncol = np.shape(df)[1]
-    months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"]
+        
 
-    clean_df = pd.read_excel(sheet, sheet_name="Sales", header = header_idx, nrows = footer_idx-header_idx, usecols=range(np.shape(df)[1])[1:])
+        if len(sheet) > 1:
+            dfList = []
+            for i,k in enumerate(sheet):
+                df = pd.read_excel(sheet[i], sheet_name="Sales", header = None)
+                header_idx = df[df.eq("Item").any(1)].index.values[0]
+                footer_idx = df[df.eq("Total").any(1)].index.values[0]-1
+                ncol = np.shape(df)[1]
+                clean_df = pd.read_excel(sheet[i], sheet_name="Sales", header = header_idx, nrows = footer_idx-header_idx, usecols=range(np.shape(df)[1])[1:])
+                dfList.append(clean_df)
+            
+            clean_df = pd.concat(dfList, axis=0)
+        else:  
+            df = pd.read_excel(sheet[0], sheet_name="Sales", header = None)
+            header_idx = df[df.eq("Item").any(1)].index.values[0]
+            footer_idx = df[df.eq("Total").any(1)].index.values[0]-1
+            ncol = np.shape(df)[1]
+            clean_df = pd.read_excel(sheet[0], sheet_name="Sales", header = header_idx, nrows = footer_idx-header_idx, usecols=range(np.shape(df)[1])[1:])
+
+        filtyear = ca.selectbox("Select Year:", ["All"] + [str(i) for i in np.unique(clean_df.loc[:, "Sale Date"].dt.year)])
+        mList = ["All"] + [months[i-1] for i in np.unique(clean_df.loc[:, "Sale Date"].dt.month)]
     
-    filtcol1 = ca.selectbox("Filter Column:", ["None","Item Type", "Venue", "Sale Date"])
-    if filtcol1 == "None":
-        filtrule1 = cb.selectbox("Filter Rule:", [])
-    elif filtcol1 in ["Item Type", "Venue"]:
-        filtrule1 = cb.selectbox("Filter Rule:", ["is", "is not"])
-    else:
-        filtrule1 = cb.selectbox("Filter Rule:", ["equals", "less than", "greater than"])
+        filtmonth = cb.selectbox("Select Month:", mList)
 
-    if filtcol1 == "None":
-        filtby1 = cc.selectbox("Filter by:", [])
-    elif filtcol1 in ["Item Type", "Venue"]:
-        filtby1 = cc.selectbox("Filter by:", np.unique(clean_df.loc[:, filtcol1]))
-    else:
-        filtby1 = cc.selectbox("Filter by:", months)
+        if (filtyear != "All") & (filtmonth == "All"):
+            sub_df = clean_df[clean_df["Sale Date"].dt.year == int(filtyear)]
+            track_df = clean_df[clean_df["Sale Date"].dt.year == int(filtyear)]
+        elif (filtyear != "All") & (filtmonth != "All"):
+            sub_df = clean_df[clean_df["Sale Date"].dt.year == int(filtyear)]
+            track_df = sub_df
+            sub_df = sub_df[sub_df["Sale Date"].dt.month == months.index(filtmonth)+1]
+        else:
+            sub_df = clean_df
+            track_df = clean_df
+        
+        c1.markdown("## Analyze Revenue Source")
+        srce = c1.selectbox("Select source:", ["Item Type", "Venue"])
+
+        fig = px.pie(sub_df, values='Net Revenue', names=srce, title='Net Revenue by Item Type')
+
+        c1.plotly_chart(fig, theme=None, use_container_width=True)
+
+        c3.markdown("## Revenue Over Time")
+        options = c3.multiselect(
+            'Select Years to Plot',
+            [str(i) for i in np.unique(clean_df.loc[:, "Sale Date"].dt.year)])
+
+        clean_df.loc[:, "year"] = [str(i) for i in clean_df.loc[:, "Sale Date"].dt.year]
+        gdf = clean_df.groupby("year")
+        maxval = 0
+        cmap=['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52']
+
+        fig2 = px.line(
+            labels={
+                "x": "Month",
+                "y": "Net Revenue",
+                "year": "year"
+             })
+        fig2.update_xaxes(
+            color="#FFF",
+            tickvals = [1,2,3,4,5,6,7,8,9,10,11,12],
+            ticktext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            )
+        fig2.update_yaxes(color="#FFF")
+
+        for i in range(len(options)):
+            t = gdf.get_group(options[i])
+            sales_summary = t.groupby(t.loc[:, "Sale Date"].dt.month)['Net Revenue'].sum().reset_index()
+            sales_summary.loc[:, "year"] = options[i]
+
+            if np.max(sales_summary.loc[:, "Net Revenue"]) > maxval:
+                maxval = np.max(sales_summary.loc[:, "Net Revenue"])
+
+
+            if i == 0:
+                fig2.add_trace(px.line(sales_summary, x=sales_summary.loc[:, "Sale Date"], y=sales_summary.loc[:, "Net Revenue"], markers=True, color=px.Constant(cmap[i]), 
+                color_discrete_map="identity").data[0])
+                
+            else:
+                fig2.add_trace(px.line(sales_summary, x=sales_summary.loc[:, "Sale Date"], y=sales_summary.loc[:, "Net Revenue"], markers=True, color=px.Constant(cmap[i]), 
+                    color_discrete_map="identity").data[0])
+            
+        fig2.update_traces(marker_size=15)
+
+        fig2.update_layout(
+                {
+                'plot_bgcolor': 'rgba(0,0,0,0)',
+                },
+                yaxis_range=[0,1.1*maxval],
+            )
+
+        c3.plotly_chart(fig2, theme=None, use_container_width=True)
+
+        c5.markdown("## Top Selling Items")
+        maxn = c5.number_input("How many top sellers would you like to see?", min_value=0, value= 10)
+
+        temp = sub_df.copy(deep=True)
+        temp.loc[temp.loc[:, "Venue"] == "Commission", "Item"] = "Commission"
+        unit_summary = temp.groupby(temp.loc[:,"Item"])['Number Sold'].sum().reset_index()
+        unit_summary = unit_summary.sort_values('Number Sold', ascending=False)
+        unit_summary = unit_summary.head(maxn)
+        fig3 = px.bar(unit_summary, x='Item', y='Number Sold',
+            labels={
+                        "Item": "",
+                        "Number Sold": "Number Sold",
+                    })
+        fig3.update_xaxes(tickangle = 90,automargin = True, color="#FFF")
+        fig3.update_yaxes(color="#FFF")
+        
+        fig3.update_layout({
+            'plot_bgcolor': 'rgba(0,0,0,0)'
+        })
+        c5.plotly_chart(fig3, theme=None, use_container_width=True)
+
+        sub_df
+
+        st.markdown("## Gross Revenue to date: " + "$" + str(np.round(np.sum(clean_df.loc[:, "Gross Revenue"]), 2)))
+        st.markdown("## :green[Net Revenue to date:] " + "$" + str(np.round(np.sum(clean_df.loc[:, "Net Revenue"]), 2)))
+        
     
-    # Analyze Revenue Source
-    c1.markdown("## Analyze Revenue Source")
-
-
-    srce = c1.selectbox("Select source:", ["Item Type", "Venue"])
-    clean_df
-    
-
-    fig = px.pie(clean_df, values='Net Revenue', names=srce, title='Net Revenue by Item Type')
-
-    c1.plotly_chart(fig, theme=None, use_container_width=True)
-
-    c3.markdown("## Revenue Over Time")
-    split_check = c3.checkbox("Split Items", value=False, key=None)
-  
-    sales_summary = clean_df.groupby(clean_df.loc[:, "Sale Date"].dt.month)['Net Revenue'].sum().reset_index()
-    fig2 = px.line(sales_summary, x=sales_summary.loc[:, "Sale Date"], y=sales_summary.loc[:, "Net Revenue"], markers=True, 
-    labels={
-                     "x": "Month",
-                     "y": "Net Revenue",
-                 })
-
-    fig2.update_layout(
-        {
-        'plot_bgcolor': 'rgba(0,0,0,0)',
-    },
-    yaxis_range=[0,1.1*np.max(sales_summary.loc[:, "Net Revenue"])]
-    )
-    fig2.update_xaxes(
-        color="#FFF",
-        tickvals = [1,2,3,4,5,6,7,8,9,10,11,12],
-        ticktext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        )
-    fig2.update_yaxes(color="#FFF")
-    fig2.update_traces(marker_size=15)
-
-    c3.plotly_chart(fig2, theme=None, use_container_width=True)
-
-    c5.markdown("## Top Selling Items")
-    maxn = c5.number_input("How many top sellers would you like to see?", min_value=0, value= 10)
-
-    temp = clean_df.copy(deep=True)
-    temp.loc[temp.loc[:, "Venue"] == "Commission", "Item"] = "Commission"
-    unit_summary = temp.groupby(temp.loc[:,"Item"])['Number Sold'].sum().reset_index()
-    unit_summary = unit_summary.sort_values('Number Sold', ascending=False)
-    unit_summary = unit_summary.head(maxn)
-    fig3 = px.bar(unit_summary, x='Item', y='Number Sold',
-        labels={
-                     "Item": "",
-                     "Number Sold": "Number Sold",
-                 })
-    fig3.update_xaxes(tickangle = 90,automargin = True, color="#FFF")
-    fig3.update_yaxes(color="#FFF")
-    
-    fig3.update_layout({
-        'plot_bgcolor': 'rgba(0,0,0,0)'
-    })
-    c5.plotly_chart(fig3, theme=None, use_container_width=True)
-
-    st.markdown("## Net Revenue to date: " + "$" + str(np.round(np.sum(clean_df.loc[:, "Net Revenue"]), 2)))
+    with tab2:
+        st.write("In Progress")
    
